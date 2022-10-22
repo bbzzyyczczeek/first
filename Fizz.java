e7ccd880d21a1ab60d12a15dbda9349a8e102986
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2022-10-22 20:19:05 +0200

    new

--- Fizz.java
+++ Fizz.java
@@ -4,8 +4,11 @@ public class Fizz {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj zakres liczb do sprawdzenia");
+
+        System.out.println("Podaj zakres liczb do sprawdzenia podzielnych przez 3i5  3 oraz 5");
+        System.out.println("Podaj pierwsza liczbe");
         int a = sc.nextInt();
+        System.out.println("Podaj druga liczbe");
         int b = sc.nextInt();
         for (int i = a; i <= b; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
